cc4a5219712de9b8d8110926cf0207988f2d652d
Dmitry Krasiev <dmitriy.krasiev@gost-group.com>
2023-12-05 18:23:07 +0400

    feat: add rxjs component

---
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-rxjs',
+  standalone: true,
+  template: ` <p>rxjs works!</p> `,
+})
+export class RxjsComponent {
+  constructor() { }
+}
